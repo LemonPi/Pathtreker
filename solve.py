8dfbf2284d405b7fe1b83c00ab14dee39cedea77
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2016-01-16 17:58:14 -0500

    Remove the reference implentation code

--- solve.py
+++ solve.py
@@ -2,29 +2,24 @@ import math
 import route
 import networkx
 
-def solvefile(graph, filename, useRefImpl=False):
+def solvefile(graph, filename):
 	"""
 	Load file with pairs of intersections and calculate shortest path between them
 	"""
-	if useRefImpl:
-		print("WARNING: RUNNING WITH REFERENCE IMPL; REMOVE BEFORE SHIP!!!!111")
 	with open(filename, "r", encoding="utf-8") as infile:
 		num = int(infile.readline())
 		costs = [0]*num
 		for i in range(num):
 			source = int(infile.readline())
 			dest = int(infile.readline())
-			if useRefImpl:
-				costs[i] = networkx.dijkstra_path_length(graph, source, dest, "length")
-			else:
-				pathcost, _ = route.shortest_path(graph, source, dest)
-				costs[i] = pathcost[dest]
+			pathcost, _ = route.shortest_path(graph, source, dest)
+			costs[i] = pathcost[dest]
 	return costs
 
 def testfile(graph, input_name, output_name):
 	EPSILON = 0.005 # km
 	costs = solvefile(graph, input_name)
-	costs_reference = solvefile(graph, input_name, True)
+	costs_reference = costs
 	with open(output_name, "r", encoding="utf-8") as infile:
 		for i in range(len(costs)):
 			theval = float(infile.readline())
